5fb459421771b845cbdb11a7949b7bbea91e3e09
hyein89 <bihontai@gmail.com>
2025-07-25 19:10:02 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,4 +1,3 @@
-// pages/[...postpath].tsx
 import React from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
@@ -8,31 +7,31 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const endpoint = process.env.GRAPHQL_ENDPOINT as string;
   const graphQLClient = new GraphQLClient(endpoint);
   const referringURL = ctx.req.headers?.referer || null;
+  const pathArr = ctx.query.postpath as string[]; // ex: ['helo']
+  const path = pathArr.join('/'); // => "helo"
   const fbclid = ctx.query.fbclid;
 
-  const pathArr = ctx.params?.postpath as string[];
-  const path = pathArr?.join('/') || '';
-
-  // Redirect jika dari Facebook
+  // Redirect if Facebook
   if (referringURL?.includes('facebook.com') || fbclid) {
     return {
       redirect: {
-        destination: `${endpoint.replace(/\/graphql\/?/, '/')}${encodeURI(path)}`,
         permanent: false,
+        destination: `${endpoint.replace('/graphql', '')}/${encodeURI(path)}/`,
       },
     };
   }
 
+  // GraphQL query pakai variable
   const query = gql`
     query GetPost($uri: ID!) {
       post(id: $uri, idType: URI) {
         id
-        excerpt
         title
-        link
+        excerpt
+        content
         dateGmt
         modifiedGmt
-        content
+        link
         author {
           node {
             name
@@ -48,19 +47,18 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     }
   `;
 
-  const variables = { uri: `/${path}/` };
-
   try {
+    const variables = { uri: `/${path}/` };
     const data = await graphQLClient.request(query, variables);
 
-    if (!data?.post) {
+    if (!data.post) {
       return { notFound: true };
     }
 
     return {
       props: {
-        post: data.post,
         path,
+        post: data.post,
         host: ctx.req.headers.host,
       },
     };
@@ -72,36 +70,40 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 interface PostProps {
   post: any;
-  path: string;
   host: string;
+  path: string;
 }
 
-const Post: React.FC<PostProps> = ({ post, path, host }) => {
+const Post: React.FC<PostProps> = ({ post, host, path }) => {
   const removeTags = (str: string) =>
-    str?.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '') || '';
+    str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
 
   return (
     <>
       <Head>
         <title>{post.title}</title>
         <meta property="og:title" content={post.title} />
+        <link rel="canonical" href={`https://${host}/${path}`} />
         <meta property="og:description" content={removeTags(post.excerpt)} />
         <meta property="og:url" content={`https://${host}/${path}`} />
-        <meta property="og:image" content={post.featuredImage?.node.sourceUrl || ''} />
-        <meta property="og:image:alt" content={post.featuredImage?.node.altText || post.title} />
         <meta property="og:type" content="article" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content={host.split('.')[0]} />
         <meta property="article:published_time" content={post.dateGmt} />
         <meta property="article:modified_time" content={post.modifiedGmt} />
-        <link rel="canonical" href={`https://${host}/${path}`} />
+        <meta property="og:image" content={post.featuredImage?.node.sourceUrl} />
+        <meta
+          property="og:image:alt"
+          content={post.featuredImage?.node.altText || post.title}
+        />
       </Head>
-      <div>
+      <div className="post-container">
         <h1>{post.title}</h1>
-        {post.featuredImage?.node?.sourceUrl && (
-          <img src={post.featuredImage.node.sourceUrl} alt={post.featuredImage.node.altText} />
-        )}
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        <img
+          src={post.featuredImage?.node.sourceUrl}
+          alt={post.featuredImage?.node.altText || post.title}
+        />
+        <article dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
     </>
   );
