c66516ba86b5896f6c7471031b81804cc4b758d3
hyein89 <bihontai@gmail.com>
2025-07-25 22:54:49 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -14,14 +14,12 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 	// redirect if facebook is the referer or request contains fbclid
 	if (referringURL?.includes('facebook.com') || fbclid) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-				}`,
-			},
-		};
+return {
+	redirect: {
+		permanent: false,
+		destination: `https://${ctx.req.headers.host}/${encodeURIComponent(path)}`
+	}
+};
 	}
 	const query = gql`
 		{
