9fb168e21f8b628a448f379e9961a20b923001c3
hyein89 <bihontai@gmail.com>
2025-07-25 08:05:50 +0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: 'https://example.com/target-article', // GANTI dengan URL tujuan
+      permanent: false,
+    },
+  };
+}
 
+export default function RedirectPage() {
+  return null;
+}
