3cace13522a5d3c07fe4b0c5083b5062abd5a9ff
hyein89 <bihontai@gmail.com>
2025-07-26 01:52:26 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,3 +1,74 @@
+// pages/[...postpath].tsx
+
+import React from 'react';
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import { GraphQLClient, gql } from 'graphql-request';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+  const graphQLClient = new GraphQLClient(endpoint);
+  const referringURL = ctx.req.headers?.referer || null;
+  const pathArr = ctx.query.postpath as Array<string>;
+  const path = pathArr.join('/');
+  const fbclid = ctx.query.fbclid;
+
+  // Redirect ke domain utama jika dari Facebook
+  if (referringURL?.includes('facebook.com') || fbclid) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `${process.env.NEXT_PUBLIC_SITE_URL}/${encodeURIComponent(path)}`,
+      },
+    };
+  }
+
+  const query = gql`
+    {
+      post(id: "/${path}/", idType: URI) {
+        id
+        excerpt
+        title
+        link
+        dateGmt
+        modifiedGmt
+        content
+        author {
+          node {
+            name
+          }
+        }
+        featuredImage {
+          node {
+            sourceUrl
+            altText
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    const data = await graphQLClient.request(query);
+
+    if (!data?.post) {
+      return { notFound: true };
+    }
+
+    return {
+      props: {
+        path,
+        post: data.post,
+        host: ctx.req.headers.host || process.env.NEXT_PUBLIC_SITE_URL,
+      },
+    };
+  } catch (error) {
+    console.error('GraphQL ERROR:', error);
+    return { notFound: true };
+  }
+};
+
+interface PostProps {
   post: any;
   host: string;
   path: string;
