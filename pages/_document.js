ed3174b82cca1219aaafef854ce6a098a0aae6d2
hyein89 <bihontai@gmail.com>
2025-07-25 08:30:29 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-         <meta property="og:url" content="https://red-cot.vercel.app" />
-        <meta property="og:image" content="https://red-cot.vercel.app/preview.jpg" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content=" " />
         <meta property="og:description" content=" " />
-        <meta property="og:type" content="website" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="https://red-cot.vercel.app" />
+        <meta property="og:image" content="https://red-cot.vercel.app/preview.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <body>
         <Main />
